80500333ac1c66d841ff9f62434cbba642f78a77
rkavi001 <rkaviarasan442@gmail.com>
2025-01-08 02:28:40 +0530

    now connected to mongodb atlas successfully

--- server/index.js
+++ server/index.js
@@ -1,51 +1,60 @@
- const express= require('express')
- const mongoose = require('mongoose')
- const cors = require('cors')
- const UserModel = require('./models/Users')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const UserModel = require('./models/Users');
 
- const app= express()
- app.use(cors())
- app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
- mongoose.connect("mongodb://127.0.0.1:27017/crud")
+// URL-encoded password for MongoDB connection string
+const dbPassword = '%23Rkavi470%23';  // Your encoded password
+const dbUser = 'rkaviarasan442';  // Your MongoDB username
+const dbCluster = 'cluster0.djugb.mongodb.net';  // Your MongoDB Atlas cluster
+const dbName = 'your_db_name';  // Your MongoDB database name
 
- app.get('/', (req,res)=> {
+// MongoDB connection string
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@${dbCluster}/${dbName}?retryWrites=true&w=majority&appName=Cluster0`)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
   UserModel.find({})
-  .then(users => res.json(users))
-  .catch(err => res.json(err))
- })
+    .then(users => res.json(users))
+    .catch(err => res.json(err));
+});
 
- app.get('/getUser/:id',(req,res)=>{
-  const id =req.params.id;
-  UserModel.findById({_id:id})
-  .then(users => res.json(users))
-  .catch(err => res.json(err))
- } )
+app.get('/getUser/:id', (req, res) => {
+  const id = req.params.id;
+  UserModel.findById({ _id: id })
+    .then(users => res.json(users))
+    .catch(err => res.json(err));
+});
 
- app.put('/updateUser/:id',(req,res) => {
-  const id =req.params.id;
-  UserModel.findByIdAndUpdate({_id:id}, {
-    name: req.body.name, 
-    email:req.body.email, 
-    age: req.body.age})
-  .then(users => res.json(users))
-  .catch(err => res.json(err))
- })
+app.put('/updateUser/:id', (req, res) => {
+  const id = req.params.id;
+  UserModel.findByIdAndUpdate({ _id: id }, {
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age
+  })
+    .then(users => res.json(users))
+    .catch(err => res.json(err));
+});
 
- app.delete('/deleteUser/:id', (req, res) => {
+app.delete('/deleteUser/:id', (req, res) => {
   const id = req.params.id;
   UserModel.findByIdAndDelete(id)
-    .then(() => res.json({ message: "User Deleted Successfully" }))
+    .then(() => res.json({ message: 'User Deleted Successfully' }))
     .catch((err) => res.json(err));
 });
 
- 
- app.post("/createUser", (req, res) => {
-   UserModel.create(req.body)
-   .then(users=> res.json(users))
-   .catch(err=>res.json(err))
- })
+app.post('/createUser', (req, res) => {
+  UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err));
+});
 
- app.listen(3001, () => {
-    console.log("server is running")
- })
+app.listen(3001, () => {
+  console.log('server is running');
+});
